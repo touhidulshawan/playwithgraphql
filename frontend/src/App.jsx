b458e93c38c94deb825badeb843bbe8bb2d06445
Touhidul Shawan <touhidulshawan@gmail.com>
2021-06-22 10:16:36 +0600

    setup apollo/client for graphql

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import Books from "./components/Books";
+
+const App = () => {
+  return (
+    <div>
+      <header>
+        <h1>Reading Books</h1>
+      </header>
+      <main>
+        <section>
+          <Books />
+        </section>
+      </main>
+    </div>
+  );
+};
+export default App;
---
+++ frontend/src/components/Books.jsx
@@ -0,0 +1,10 @@
+const Books = () => {
+  return (
+    <div>
+      <ul>
+        <li>Book Name</li>
+      </ul>
+    </div>
+  );
+};
+export default Books;
